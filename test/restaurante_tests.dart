667faa55febc4dc624fbf7e5be23ae0c6d846e24
Otávio Augusto Padilha de Campos <otavioaugcampos08@gmail.com>
2025-11-24 21:42:31 -0300

    Update restaurante_tests.dart

--- test/restaurante_tests.dart
+++ test/restaurante_tests.dart
@@ -1,6 +1,6 @@
 // Arquivo: test/restaurante_tests.dart
 import 'package:test/test.dart'; 
-import 'package:pipeline-teste/lib/restaurante_validator.dart';
+import 'package:pipeline_teste/restaurante_validator.dart';
 
 void main() {
   group('Testes Unitários de Regras de Negócio Robustas (TUCs)', () {
