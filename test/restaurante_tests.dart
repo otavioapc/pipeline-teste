e1b3c696a37943a20043fe325f40c7063879cd75
Otávio Augusto Padilha de Campos <otavioaugcampos08@gmail.com>
2025-11-24 21:46:37 -0300

    Update restaurante_tests.dart

--- test/restaurante_tests.dart
+++ test/restaurante_tests.dart
@@ -1,11 +1,11 @@
 // Arquivo: test/restaurante_tests.dart
 import 'package:test/test.dart'; 
-import 'package:pipeline_teste/restaurante_validator.dart';
+import 'package:pipeline_teste/restaurante_validator.dart'; // Correção de Sintaxe (usa underscore)
 
 void main() {
   group('Testes Unitários de Regras de Negócio Robustas (TUCs)', () {
 
-    // TUC01 - Nome (Análise de Valor-Limite: Limite Excedido - 101 caracteres)
+    // TUC01 - Nome (Análise de Valor-Limite: Limite Excedido)
     test('TUC01: Nome com 101 caracteres deve retornar FALSE', () {
       String nomeExcedido = 'A' * 101; 
       expect(validarLimiteTexto(nomeExcedido, 100), isFalse);
@@ -13,7 +13,6 @@ void main() {
 
     // TUC02 - CNPJ (Classe de Equivalência: Sequência Repetida - Inválida)
     test('TUC02: CNPJ com 14 zeros deve retornar FALSE', () {
-        // Testa uma Classe de Equivalência Inválida comum (000.000.0000-00)
         expect(validarCNPJDados('00000000000000'), isFalse);
     });
 
@@ -24,13 +23,11 @@ void main() {
 
     // TUC04 - E-mail Válido (Teste de Formato - Classe Válida)
     test('TUC04: E-mail formatado corretamente deve retornar TRUE', () {
-        // Prova que a função Regex está funcionando
         expect(validarFormatoEmail('restaurante@foodpass.com'), isTrue);
     });
 
     // TUC05 - E-mail Inválido (Teste de Formato - Classe Inválida)
     test('TUC05: E-mail sem o "@" deve retornar FALSE', () {
-        // Prova que a validação de formato básico está funcionando (CEI)
         expect(validarFormatoEmail('restaurantefoodpass.com'), isFalse); 
     });
   });
